ea7284851a2d5c3dcac1c5cb027893dbb4b5a9d6
d3fau1t <aciddust@users.noreply.github.com>
2025-02-13 11:34:53 +0900

    fix(asmr): screen height

--- web/src/routes/asmr/+page.svelte
+++ web/src/routes/asmr/+page.svelte
@@ -381,7 +381,7 @@
 </script>
 
 <div class="flex">
-  <div style="width: 100vw; height: 80vh;" bind:this={container}>
+  <div style="width: 100vw; height: 75vh;" bind:this={container}>
     {#if loading}
       <div class="flex justify-center items-center h-full">
         <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
