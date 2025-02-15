cfd4bc1627c46eb1d742f1f7c7269ce4a339b8c0
d3fau1t <aciddust@users.noreply.github.com>
2025-02-15 16:16:13 +0900

    fix(asmr): modify sound source

--- web/src/routes/asmr/soundButton.svelte
+++ web/src/routes/asmr/soundButton.svelte
@@ -62,7 +62,7 @@
         scrolling="yes"
         frameborder="no"
         allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/300494469&color=%23252503&auto_play=true&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false">
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/751456812&color=%23252503&auto_play=true&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false">
       </iframe>
       <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
         <a href="https://soundcloud.com/chillhopdotcom" title="" target="_blank" style="color: #cccccc; text-decoration: none;"></a>
