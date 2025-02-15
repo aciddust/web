fd3b44dd55977601a54893c64fdfb1b702749d59
d3fau1t <aciddust@users.noreply.github.com>
2025-02-15 17:19:19 +0900

    fix(asmr): metatag
    asmr -> murmaverse (murmur + universe)

--- web/src/routes/asmr/+layout.svelte
+++ web/src/routes/asmr/+layout.svelte
@@ -1,1 +1,11 @@
+<title>Murmaverse</title>
+<meta property="twitter:image" content="https://web-ashen-psi.vercel.app/asmr/preview.png">
+<meta property="twitter:title" content="Murmaverse">
+<meta property="twitter:description" content="여러 사운드를 조합해보세요">
+<meta property="description" content="여러 사운드를 조합해 원하는 소리를 찾아보세요" />
+<meta property="og:image" content="https://web-ashen-psi.vercel.app/asmr/preview.png">
+<meta property="og:title" content="Murmaverse">
+<meta property="og:description" content="여러 사운드를 조합해 원하는 소리를 찾아보세요" />
+<meta property="og:url" content="https://web-ashen-psi.vercel.app/asmr">
+
 <slot />
