af70cd782a8ffd4bf4394499046050d65cd8d0c0
d3fau1t <aciddust@users.noreply.github.com>
2025-02-22 13:17:55 +0900

    fix(feedback): example, content

--- web/src/routes/misc/feedback/+page.svelte
+++ web/src/routes/misc/feedback/+page.svelte
@@ -10,6 +10,14 @@
   const email = writable('');
   const content = writable('');
 
+  const contentExample = `
+  사용중 불편한 점이 있거나 원하시는 개선 사항이 있으시면 입력해주세요.
+  가능한 빠르게 반영하도록 노력하겠습니다.
+
+  If you have any problems or suggestions for improvement, please let us know.
+  We'll try to address them as soon as possible.
+  `;
+
   const sendFeedback = async (data: { name: string; email: string; content: string }) => {
     const response = await fetch('/api/feedback', {
       method: 'POST',
@@ -70,18 +78,27 @@
     <div class="flex w-full space-x-4">
       <div class="flex w-full flex-col space-y-2">
         <h2 class="text-lg font-bold">Name</h2>
-        <Input id="name" type="text" class="input" bind:value={$name} />
+        <Input
+          placeholder="김햄부기온앤온"
+          id="name" type="text" class="input" bind:value={$name}
+        />
       </div>
       <div class="flex w-full flex-col space-y-2">
         <h2 class="text-lg font-bold">Email</h2>
         <!-- validate email -->
-        <Input id="email" type="email" class="input" bind:value={$email} />
+        <Input
+          placeholder="example@email.com"
+          id="email" type="email" class="input" bind:value={$email}
+        />
       </div>
     </div>
     <div class="flex w-full space-x-4">
       <div class="flex w-full flex-col space-y-2">
         <h2 class="text-lg font-bold">Content</h2>
-        <Textarea id="content" class="input h-80" bind:value={$content} />
+        <Textarea
+            placeholder={contentExample}
+          id="content" class="input h-80" bind:value={$content}
+        />
       </div>
     </div>
     <Button on:click={submitFeedback}>Submit</Button>
