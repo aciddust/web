74f55651c9747c6b89ebee84d684104940cd6d50
d3fau1t <aciddust@users.noreply.github.com>
2025-03-14 23:59:18 +0900

    fix(ziplink): add chrome extension link

--- web/src/routes/ziplink/+page.svelte
+++ web/src/routes/ziplink/+page.svelte
@@ -27,8 +27,21 @@
 <div class="height-full w-full flex flex-col items-center justify-center">
   <div class='w-full flex flex-col items-center justify-center'>
     <div class="flex flex-col justify-center mt-4 border border-gray-200 p-4 rounded space-y-4">
-      <div class='flex items-center justify-center'>
+      <div class='flex items-center justify-center space-x-2'>
         <h1 class='text-4xl font-bold'>ZipLink</h1>
+        <button 
+          type="button"
+          on:click={() => window.open('https://chromewebstore.google.com/detail/kjclbbdiofgkkpncjhfmhaoiboigmobb?utm_source=item-share-cb', '_blank')}
+          on:keydown={(e) => e.key === 'Enter' && window.open('https://chromewebstore.google.com/detail/kjclbbdiofgkkpncjhfmhaoiboigmobb?utm_source=item-share-cb', '_blank')}
+          aria-label="Open Chrome Extension"
+          class="bg-transparent border-none cursor-pointer p-0">
+          <svg
+            class='w-8 h-8 text-blue-500'
+            role="img"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"><title>Google Chrome</title><path d="M12 0C8.21 0 4.831 1.757 2.632 4.501l3.953 6.848A5.454 5.454 0 0 1 12 6.545h10.691A12 12 0 0 0 12 0zM1.931 5.47A11.943 11.943 0 0 0 0 12c0 6.012 4.42 10.991 10.189 11.864l3.953-6.847a5.45 5.45 0 0 1-6.865-2.29zm13.342 2.166a5.446 5.446 0 0 1 1.45 7.09l.002.001h-.002l-5.344 9.257c.206.01.413.016.621.016 6.627 0 12-5.373 12-12 0-1.54-.29-3.011-.818-4.364zM12 16.364a4.364 4.364 0 1 1 0-8.728 4.364 4.364 0 0 1 0 8.728Z"/>
+          </svg>
+        </button>
       </div>
       <div class='flex flex-col space-y-1'>
         <Input
